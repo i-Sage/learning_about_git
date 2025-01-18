89af320d749bab1ab3a3e80cc296827c7bf47782
i-Sage <sage.on.sage.26666@gmail.com>
2025-01-18 14:13:19 +0100

    implemented the generator

--- src/generator.cc
+++ src/generator.cc
@@ -3,8 +3,22 @@
 /// stding
 
 #include <fstream>
+#include <random>
+
 
 int main()
 {
+    std::ofstream test_file{"test_file.txt"};
 
+    /// Set up random number engine
+    std::random_device rd;
+    std::mt19937_64 gen{rd()};
+    std::uniform_int_distribution<long long> distribution(1e7, 1e8);
+
+    for(size_t idx {0}; idx < 1e6; idx++)
+    {
+        test_file << distribution(gen) 
+                  << ' ' 
+                  << distribution(gen) << '\n';
+    }
 }
